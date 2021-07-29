1cb4528c6194fec4b600227cfd0fc1aabd34fd8e
Long <123@ex.com>
2021-07-29 22:09:15 +0800

    修正開發跟打包的路徑錯誤

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  publicPath: publicPath()
 };
+
+
+function publicPath() {
+	switch (process.env.NODE_ENV) {
+		case 'production':
+			return './';
+		default:
+			return '/';
+	}
+}
