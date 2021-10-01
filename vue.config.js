8602d660e2f8b78cb0f22e884d8a7128c4db0fba
Long <long@ebmtech.com>
2021-10-01 12:31:00 +0800

    打包路徑調整

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  lintOnSave:false
+  lintOnSave:false,
+  publicPath: publicPath()
 };
+
+
+function publicPath() {
+	switch (process.env.NODE_ENV) {
+		case 'production':
+			return './';
+		default:
+			return '/';
+	}
+}
